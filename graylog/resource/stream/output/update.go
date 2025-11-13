c2a8c6a4473bc32a239c80a7b099c49e7b8da3da
Sven Borkert <sven@borkert.net>
2025-11-13 14:29:37 +0100

    fix: Remove unused util imports and add local usage documentation
    - Fixed build errors by removing unused util imports in 3 files
    - Added comprehensive LOCAL_USAGE.md with 3 methods to use provider locally
    - Created build-local.sh script for easy build and installation
    - Added example-local-usage/ directory with working Terraform configuration
    - Updated .gitignore to exclude built binary and local env files
    
    The provider now builds successfully and can be used locally via:
    1. Local installation to ~/.terraform.d/plugins/
    2. Development overrides in .terraformrc
    3. Go install for system-wide usage

--- graylog/resource/stream/output/update.go
+++ graylog/resource/stream/output/update.go
@@ -7,7 +7,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/terraform-provider-graylog/terraform-provider-graylog/graylog/client"
 	"github.com/terraform-provider-graylog/terraform-provider-graylog/graylog/convert"
-	"github.com/terraform-provider-graylog/terraform-provider-graylog/graylog/util"
 )
 
 func update(d *schema.ResourceData, m interface{}) error {
--- graylog/resource/system/pipeline/connection/update.go
+++ graylog/resource/system/pipeline/connection/update.go
@@ -2,7 +2,6 @@ package connection
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/terraform-provider-graylog/terraform-provider-graylog/graylog/util"
 )
 
 func update(d *schema.ResourceData, m interface{}) error {
